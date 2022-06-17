51141e3a3a3357759fbc38fa4f3cb6a0ccbe7177
aravindnatch <aravindnatch@users.noreply.github.com>
2022-06-17 04:37:51 +0000

    deploy: b06a5cf1c16ec7ffbb4a7f1a035e8f4e37ae2b5c

--- _next/static/XrLkNdisObQ-YO2Dr-xX-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e6b7271cb80eb658.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/game":["static/chunks/pages/game-4a461e112bf50391.js"],sortedPages:["/","/_app","/_error","/game"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
