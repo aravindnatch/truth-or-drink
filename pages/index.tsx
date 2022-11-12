92576f80a5dacc02e5b1ec83f0af2dc6c62f5fcb
Aravind Natchiappan <aravind.natch@gmail.com>
2022-11-12 04:28:49 -0500

    remove cursor pointer from online button

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
             TRUTH <span className="text-xl align-middle">O R</span> DRINK
           </div>
 
-          <Link href='/manipulate'>
+          <Link href='/manipulate' className="cursor-default">
             <div className="select-none rounded-lg bg-[#8D889F] text-center text-white font-bold max-w-fit px-4 m-auto">
               O N L I N E
             </div>
